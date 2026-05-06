6186c7819c898fe44ad98a548cb791647e222d0e
Tien Hoang <nguyentienhoang260204@gmail.com>
2026-05-06 10:39:12 +0700

    fix: thêm hàm cookie

--- app/(main)/api/auth/callback/route.ts
+++ app/(main)/api/auth/callback/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { createClient } from '@/lib/supabase/server'
+import { createServerClient } from '@supabase/ssr'
 
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url)
@@ -7,8 +7,27 @@ export async function GET(request: NextRequest) {
   const redirect = searchParams.get('redirect') || '/'
 
   if (code) {
-    const supabase = createClient()
+    const response = NextResponse.redirect(`${origin}${redirect}`)
+    
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return request.cookies.getAll()
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              response.cookies.set(name, value, options)
+            )
+          },
+        },
+      }
+    )
+
     await supabase.auth.exchangeCodeForSession(code)
+    return response
   }
 
   return NextResponse.redirect(`${origin}${redirect}`)
